ebed478aa51c4cc6652ba594dded2bea36b06695
Anusha Sengar <116504306+anushasengar3@users.noreply.github.com>
2024-06-10 04:24:54 +0530

    Add files via upload

---
+++ q8.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+
+# In[18]:
+
+
+df=pd.read_csv('diabetes.csv')
+
+
+# In[19]:
+
+
+X=df.values
+
+
+# In[23]:
+
+
+scaler=StandardScaler()
+X_scaled=scaler.fit_transform(X)
+
+
+# In[24]:
+
+
+k_values=[2,3,4]
+
+
+# In[26]:
+
+
+for k in k_values:
+    kmeans=KMeans(n_clusters=k,random_state=0)
+    kmeans.fit(X_scaled)
+    y_kmeans=kmeans.predict(X_scaled)
+    plt.figure(figsize=(8,6))
+    plt.scatter(X_scaled[:,0],X_scaled[:,1],c=y_kmeans,s=50,cmap='viridis')
+    centers=kmeans.cluster_centers_
+    plt.scatter(centers[:,0],centers[:,1],c='red',s=200,alpha=0.75,marker='X')
+    plt.title(f'K-Means Clustering (K={k})')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.grid(True)
+    plt.show()
+
+
+# In[ ]:
+
+
+
+
